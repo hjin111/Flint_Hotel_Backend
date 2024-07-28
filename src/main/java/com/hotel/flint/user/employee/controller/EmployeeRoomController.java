c5f9dd9f1f58126a5aa4d5ca68d497818f810751
jasonhwang <junghahwang0601@gmail.com>
2024-07-28 19:52:45 +0900

    FIX: git pull 전 commit

--- src/main/java/com/hotel/flint/user/employee/controller/EmployeeRoomController.java
+++ src/main/java/com/hotel/flint/user/employee/controller/EmployeeRoomController.java
@@ -2,7 +2,6 @@ package com.hotel.flint.user.employee.controller;
 
 import com.hotel.flint.common.dto.CommonErrorDto;
 import com.hotel.flint.common.dto.CommonResDto;
-import com.hotel.flint.reserve.room.dto.RoomStateDto;
 import com.hotel.flint.user.employee.service.EmployeeRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,19 +19,6 @@ public class EmployeeRoomController {
     @Autowired
     public EmployeeRoomController(EmployeeRoomService employeeRoomService) {
         this.employeeRoomService = employeeRoomService;
-    }
-
-    @PatchMapping("/setstate/{room_id}")
-    public ResponseEntity<?> setRoomState(@PathVariable Long room_id,
-                                          @RequestBody RoomStateDto roomStateDto) {
-        try {
-            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "객실 상태가 변경되었습니다", null);
-            employeeRoomService.setRoomState(room_id, roomStateDto);
-            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
-        } catch (EntityNotFoundException e){
-            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), e.getMessage());
-            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
-        }
     }
 
     @PatchMapping("/modprice/{room_type_id}")
--- src/main/java/com/hotel/flint/user/employee/service/EmployeeRoomService.java
+++ src/main/java/com/hotel/flint/user/employee/service/EmployeeRoomService.java
@@ -1,10 +1,16 @@
 package com.hotel.flint.user.employee.service;
 
+import com.hotel.flint.common.enumdir.Option;
 import com.hotel.flint.reserve.room.domain.RoomInfo;
 import com.hotel.flint.reserve.room.repository.RoomDetailsRepository;
 import com.hotel.flint.reserve.room.repository.RoomInfoRepositoy;
 import com.hotel.flint.reserve.room.repository.RoomPriceRepository;
+import com.hotel.flint.user.employee.dto.Employee;
+import com.hotel.flint.user.employee.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,15 +22,34 @@ public class EmployeeRoomService {
     private final RoomPriceRepository roomPriceRepository;
     private final RoomDetailsRepository roomDetailsRepository;
     private final RoomInfoRepositoy roomInfoRepositoy;
-
+    private final EmployeeRepository employeeRepository;
     @Autowired
-    public EmployeeRoomService(RoomPriceRepository roomPriceRepository, RoomDetailsRepository roomDetailsRepository, RoomInfoRepositoy roomInfoRepositoy) {
+    public EmployeeRoomService(RoomPriceRepository roomPriceRepository, RoomDetailsRepository roomDetailsRepository, RoomInfoRepositoy roomInfoRepositoy, EmployeeRepository employeeRepository) {
         this.roomPriceRepository = roomPriceRepository;
         this.roomDetailsRepository = roomDetailsRepository;
         this.roomInfoRepositoy = roomInfoRepositoy;
+        this.employeeRepository = employeeRepository;
+    }
+
+    private Employee getAuthenticatedEmployee() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("인증 값::" + authentication + "\n 여기가 끝");
+        if (authentication != null && authentication.isAuthenticated()) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String email = userDetails.getUsername();
+            return employeeRepository.findByEmailAndDelYN(email, Option.N)
+                    .orElseThrow(() -> new SecurityException("인증되지 않은 사용자입니다."));
+        } else {
+            throw new SecurityException("인증되지 않은 사용자입니다.");
+        }
     }
 
     public void modRoomPrice(Long id, Double newPrice){
+        Employee authenticatedEmployee = getAuthenticatedEmployee();
+
+        if(!authenticatedEmployee.getDepartment().toString().equals("ROOM")){
+            throw new IllegalArgumentException("접근 권한이 없습니다.");
+        }
         RoomInfo roomInfo = roomInfoRepositoy.findById(id).orElseThrow(
                 ()-> new EntityNotFoundException("해당 타입의 객실은 존재하지 않습니다."));
 
