522e5557c720bcb21ee5d3e4a7c71283bc788ee2
H-Yeji <yeji980603@naver.com>
2024-07-31 23:35:36 +0900

    Feat: 요청시, 객실 예약 내역 수정 (Room담당 직원만 가능)

--- src/main/java/com/hotel/flint/user/employee/controller/EmployeeRoomController.java
+++ src/main/java/com/hotel/flint/user/employee/controller/EmployeeRoomController.java
@@ -2,6 +2,7 @@ package com.hotel.flint.user.employee.controller;
 
 import com.hotel.flint.common.dto.CommonErrorDto;
 import com.hotel.flint.common.dto.CommonResDto;
+import com.hotel.flint.user.employee.dto.EmployeeModRoomDto;
 import com.hotel.flint.user.employee.dto.InfoRoomResDto;
 import com.hotel.flint.user.employee.service.EmployeeRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -67,4 +68,22 @@ public class EmployeeRoomController {
             return new ResponseEntity<>(commonErrorDto, HttpStatus.FORBIDDEN);
         }
     }
+
+    /**
+     * 고객의 요청시, 객실 예약내역 수정
+     */
+    @PostMapping("/modify/{id}") // reserve_id
+    public ResponseEntity<?> memberReservationModRoomByEmployee(@PathVariable Long id, @RequestBody EmployeeModRoomDto dto) {
+
+        try {
+            employeeRoomService.updateRoomReservation(id, dto);
+
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "객실 예약 내역 수정 완료", null);
+            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+        } catch (EntityNotFoundException | IllegalArgumentException e) {
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
