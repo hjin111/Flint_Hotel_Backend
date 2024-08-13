d8858c1b2ced46c212eec8700959cba3531ada7d
H-Yeji <yeji980603@naver.com>
2024-08-14 01:32:38 +0900

    Fix: Qna엔티티 필드 수정 & Qna관련 dto에 id 필드 추가

--- src/main/java/com/hotel/flint/support/qna/dto/QnaDetailDto.java
+++ src/main/java/com/hotel/flint/support/qna/dto/QnaDetailDto.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class QnaDetailDto {
 
+    private Long id;
     private Service service;
     private String title;
     private String contents;
